bd5794180d12424521a86584bbb63dc0865ccaed
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-09-25 14:17:54 -0300

    Torneos Fragment Listo

---
+++ ampaMain/app/src/main/java/com/example/ampamain/ui/home/Torneos.java
@@ -0,0 +1,26 @@
+package com.example.ampamain.ui.home;
+
+public class Torneos {
+    private final String title;
+    private final String description;
+
+    private final int imageResource;
+
+    public Torneos(String title, String description, int imageResource) {
+        this.title = title;
+        this.description = description;
+        this.imageResource = imageResource;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+}
--- ampaMain/app/src/main/java/com/example/ampamain/ui/home/tabbed/torneosFragment.java
+++ ampaMain/app/src/main/java/com/example/ampamain/ui/home/tabbed/torneosFragment.java
@@ -1,66 +1,42 @@
 package com.example.ampamain.ui.home.tabbed;
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.ampamain.R;
+import com.example.ampamain.ui.home.Torneos;
+import com.example.ampamain.ui.home.TournamentsAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link torneosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class torneosFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public torneosFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment torneosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static torneosFragment newInstance(String param1, String param2) {
-        torneosFragment fragment = new torneosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_torneos, container, false);
+        View view = inflater.inflate(R.layout.fragment_torneos, container, false);
+
+        // Create a list of predefined tournaments
+        List<Torneos> torneosList = new ArrayList<>();
+        torneosList.add(new Torneos("Torneo 1", "Fecha: 21/09/2023, inscripciones hasta: 20/10/2023, Costo por equipo:$20000, para mas informacion contáctenos", R.drawable.torneo1));
+        torneosList.add(new Torneos("Torneo 2", "Fecha: 21/09/2023, inscripciones hasta: 20/10/2023, Costo por equipo:$20000, para mas informacion contáctenos", R.drawable.torneo2));
+        torneosList.add(new Torneos("Torneo 3", "Fecha: 21/09/2023, inscripciones hasta: 20/10/2023, Costo por equipo:$20000, para mas informacion contáctenos", R.drawable.torneo3));
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.tournaments_recycler_view);
+        TournamentsAdapter adapter = new TournamentsAdapter(getContext(), torneosList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+        return view;
     }
 }
