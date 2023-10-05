18b53f0b03b033cfdfab9ab807a94938423f7205
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-10-05 13:53:24 -0300

    Logout implementado, y cambio Activity Welcome

--- ampaMain/app/src/main/java/com/example/ampamain/welcome.java
+++ ampaMain/app/src/main/java/com/example/ampamain/welcome.java
@@ -1,28 +1,37 @@
 package com.example.ampamain;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class welcome extends AppCompatActivity {
+
+    // Duración en milisegundos que mostrarás la pantalla de bienvenida
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;  // 2 segundos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        Button buttonToLogin = findViewById(R.id.btnLogin_Login);
-        buttonToLogin.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-               // Intent para iniciar LoginActivity
-                Intent intent = new Intent(welcome.this, login.class);
-                // Inicia LoginActivity
-                startActivity(intent);
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (currentUser != null) {
+                    // Si el usuario ya está autenticado, redirigimos a MainActivity
+                    Intent intent = new Intent(welcome.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Si el usuario no está autenticado, redirigimos a LoginActivity
+                    Intent intent = new Intent(welcome.this, login.class);
+                    startActivity(intent);
+                }
+                // Finaliza la actividad Welcome para que el usuario no pueda regresar a ella con el botón atrás
+                finish();
             }
-        });
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
