204c4752e2ea9b1990f0ad43f791a6ec690b96bf
NahuelPardo89 <106750741+NahuelPardo89@users.noreply.github.com>
2023-10-04 21:21:42 -0300

    en trabajo perfil

--- ampaMain/app/src/main/java/com/example/ampamain/ui/perfil/PerfilFragment.java
+++ ampaMain/app/src/main/java/com/example/ampamain/ui/perfil/PerfilFragment.java
@@ -1,5 +1,7 @@
 package com.example.ampamain.ui.perfil;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,57 +9,77 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
+
 import com.example.ampamain.R;
-import com.bumptech.glide.Glide;
-import com.example.ampamain.UserProfile;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class PerfilFragment extends Fragment {
 
-    private PerfilViewModel perfilViewModel;
-    private TextView dniText, nameText, emailText, isActiveText;
-    private ImageView profileImage;
+    private PerfilViewModel mViewModel;
+    private TextView nameTextView, emailTextView, dniTextView;
+    private ImageView profileImageView;
+    private FirebaseAuth mAuth;
 
-    private FirebaseUser currentUser;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        perfilViewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_perfil, container, false);
-
-        // Inicialización de los elementos de la vista
-        dniText = root.findViewById(R.id.dni);
-        nameText = root.findViewById(R.id.nombre);
-        emailText = root.findViewById(R.id.email);
-        isActiveText = root.findViewById(R.id.is_active);
-        profileImage = root.findViewById(R.id.profile_image);
-        // Obtener el usuario actual de FirebaseAuth
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        // Observar los cambios en el perfil del usuario
-        perfilViewModel.getUserProfile().observe(getViewLifecycleOwner(), this::updateUI);
-
-        // botón Editar para navegar al fragmento de edición
-        root.findViewById(R.id.edit_button).setOnClickListener(v -> {
-            // Navegar hacia el fragmento de edición (asumiendo que se llamará EditProfileFragment)
-            NavHostFragment.findNavController(this).navigate(R.id.editProfileFragment);
-        });
-
-        return root;
+    public static PerfilFragment newInstance() {
+        return new PerfilFragment();
     }
 
-    private void updateUI(UserProfile userProfile) {
-        dniText.setText("nombre"+currentUser.getDisplayName());
-        nameText.setText(userProfile.getNombre());
-        emailText.setText("email"+currentUser.getEmail());
-        isActiveText.setText(userProfile.isIsActive() ? "Activo" : "Inactivo");
-        Glide.with(this)
-                .load(userProfile.getFotoUrl())
-                .into(profileImage);
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_perfil, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
+
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Views
+        nameTextView = getView().findViewById(R.id.nombreTextView_perfil);
+        emailTextView = getView().findViewById(R.id.emailTextView_perfil);
+        profileImageView = getView().findViewById(R.id.profileImageView);
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            nameTextView.setText( user.getDisplayName());
+            emailTextView.setText( user.getEmail());
+
+            // Fetch and set user's profile picture if available
+            Uri photoUri = user.getPhotoUrl();
+            if (photoUri != null) {
+                new Thread(() -> {
+                    try {
+                        Bitmap bitmap = getBitmapFromURL(photoUri.toString());
+                        getActivity().runOnUiThread(() -> profileImageView.setImageBitmap(bitmap));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+        }
+    }
+
+    public Bitmap getBitmapFromURL(String src) throws IOException {
+        URL url = new URL(src);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);
+        connection.connect();
+        InputStream input = connection.getInputStream();
+        return BitmapFactory.decodeStream(input);
     }
 }
